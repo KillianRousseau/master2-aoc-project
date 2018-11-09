7d04bafd505fd847cfd29e41e5732788cb803ebf
Killian <killianrousseau@hotmail.fr>
2018-11-09 09:09:43 +0100

    Création du Main et implémentation de l'afficheur avec interface Java Swing

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import observer.Afficheur;
+import proxy.Canal;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		/*ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
+		Canal c1 = new Canal(scheduledExecutorService);
+		Afficheur a1 = new Afficheur(c1,0,0);
+		Afficheur a2 = new Afficheur(c1,600,0);
+		Afficheur a3 = new Afficheur(c1,0,400);
+		Afficheur a4 = new Afficheur(c1,600,400);*/
+	}
+
+}
--- src/observer/Afficheur.java
+++ src/observer/Afficheur.java
@@ -1,11 +1,64 @@
 package observer;
 
-public class Afficheur implements ObservateurCapteur{
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagLayout;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 
-	@Override
-	public void update(Capteur generateur) {
-		// TODO Auto-generated method stub
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import proxy.Canal;
+
+public class Afficheur extends JFrame implements ObservateurCapteur{
+
+	private Canal canal;
+	private String val;
+	private JPanel panel;
+	private JLabel label;
+	
+	public Afficheur(int locationX, int locationY) {
+		this(null, locationX, locationY);
+	}
+	
+	public Afficheur(Canal canal, int locationX,int locationY) {
+		this.canal = canal;
+		this.val = "";
 		
+		setTitle("Afficheur");
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    setPreferredSize(new Dimension(600, 400));
+	    setLocation(locationX, locationY);
+	    
+	    this.setLayout(new GridBagLayout());
+	    this.panel = new JPanel();
+	    this.label = new JLabel(val);
+	    this.label.setFont(new Font("Courier New", Font.BOLD, 60));
+	    this.panel.add(this.label);
+	    this.add(this.panel);
+	    pack();
+	    setVisible(true);        
+          
+	}
+	
+	@Override
+	public void update(Capteur capteur) {
+		if(this.canal != null) {
+			//Future f = this.canal.getValue();
+			//this.val = f.get().toString();
+			//this.label.setText(this.val);
+		}
+	}
+	
+	public void setCanal(Canal c) {
+		this.canal = c;
+	}
+	
+	public Canal getCanal() {
+		return this.canal;
 	}
 
 }
